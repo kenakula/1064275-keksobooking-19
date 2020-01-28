c90d116003186f12afda92874ce17b0163d1a60f
Kenan Agaev <kenkosh@yandex.ru>
2020-01-28 10:46:56 +0300

    Начинает работу над заданием

---
+++ js/main.js
@@ -0,0 +1,68 @@
+/* eslint-disable no-console */
+'use strict';
+
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
+
+var getImageLink = function () {
+  return 'img/avatars/user0' + i + '.png';
+};
+
+var getPrice = function () {
+  return Math.round(Math.random() * 2000) + ' Рублей за сутки';
+};
+
+var getType = function () {
+  var flatTypes = ['palace', 'flat', 'house', 'bungalo'];
+
+  return flatTypes[Math.floor(Math.random() * 4)];
+};
+
+var getRoomNumber = function (offer) {
+  var index;
+
+  if (offer.type === 'palace') {
+    index = 20;
+  } else if (offer.type === 'flat') {
+    index = 5;
+  } else if (offer.type === 'house') {
+    index = 10;
+  } else if (offer.type === 'bungalo') {
+    index = 3;
+  }
+
+  return Math.ceil(Math.random() * index);
+};
+debugger;
+// не работает
+
+var getGuestsNumber = function (offer) {
+  return Math.ceil(Math.random() * offer.rooms);
+}
+
+var createOffer = function () {
+  var offer = {};
+
+  offer.title = 'Заголовок предложения';
+  offer.address = '{{location.x}}, {{location.y}}';
+  offer.price = getPrice();
+  offer.type = getType();
+  offer.rooms = getRoomNumber(offer);
+  offer.guests = getGuestsNumber(offer);
+
+  return offer;
+};
+
+var createObject = function () {
+  var object = {};
+  object.author = {avatar: getImageLink()};
+  object.offer = createOffer();
+
+  return object;
+};
+
+for (var i = 1; i < 9; i++) {
+  console.log(createObject());
+}
+
+

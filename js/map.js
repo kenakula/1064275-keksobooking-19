d19ee6e7493f44a5896a025885faf9492b4de868
Kenan Agaev <kenkosh@yandex.ru>
2020-02-16 16:29:24 +0300

    Настраивает получение и отрисовку пинов с сервера

--- js/map.js
+++ js/map.js
@@ -1,11 +1,10 @@
+/* eslint-disable no-console */
 'use strict';
 
 (function () {
   var KEY_ENTER = 'Enter';
   var PIN_WIDTH = 50;
   var PIN_HEIGHT = 70;
-
-  var OFFER_COUNT = 8;
 
   var adForm = document.querySelector('.ad-form');
   var formFields = adForm.querySelectorAll('fieldset');
@@ -29,49 +28,6 @@
     return posX + ', ' + posY;
   };
 
-  // поолучает массив похожих объявлений
-  var getMocks = function (count) {
-    var result = [];
-
-    for (var i = 0; i < count; i++) {
-      result.push(window.mock.createObject(i));
-    }
-
-    return result;
-  };
-
-  // позиционирует и стилизует пин
-  var setPin = function (data, element) {
-    var img = element.querySelector('img');
-
-    element.style.left = data.offer.location.x - (PIN_WIDTH / 2) + 'px';
-    element.style.top = data.offer.location.y - PIN_HEIGHT + 'px';
-    img.src = data.author.avatar;
-    img.alt = data.offer.title;
-  };
-
-  // создает фрагмент пинов
-  var getFragment = function (data, template) {
-    var fragment = document.createDocumentFragment();
-
-    for (var i = 0; i < data.length; i++) {
-      var pinElement = template.cloneNode(true);
-      setPin(data[i], pinElement);
-      fragment.appendChild(pinElement);
-    }
-
-    return fragment;
-  };
-
-
-  // активирует страницу
-  var activatePage = function () {
-    map.classList.remove('map--faded');
-    mapArea.appendChild(pinsFragment);
-    adForm.classList.remove('ad-form--disabled');
-    changeFormFieldsState(formFields, false);
-  };
-
   var onMainPinClick = function () {
     activatePage();
   };
@@ -82,8 +38,51 @@
     }
   };
 
-  var data = getMocks(OFFER_COUNT);
-  var pinsFragment = getFragment(data, pinTemplate);
+  var renderPin = function (dataPin) {
+    var pinElement = pinTemplate.cloneNode(true);
+    var img = pinElement.querySelector('img');
+
+    pinElement.style.left = dataPin.location.x - (PIN_WIDTH / 2) + 'px';
+    pinElement.style.top = dataPin.location.y - PIN_HEIGHT + 'px';
+    img.src = dataPin.author.avatar;
+    img.alt = dataPin.offer.title;
+
+    return pinElement;
+  };
+
+  var successHandler = function (dataPins) {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < dataPins.length; i++) {
+      var getRandomPin = dataPins[i];
+
+      fragment.appendChild(renderPin(getRandomPin));
+    }
+
+    mapArea.appendChild(fragment);
+  };
+
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  // активирует страницу
+  var activatePage = function () {
+    map.classList.remove('map--faded');
+    window.backend.load(successHandler, errorHandler);
+    adForm.classList.remove('ad-form--disabled');
+    changeFormFieldsState(formFields, false);
+  };
+
   addressInput.value = getCoordinates(mainPin);
 
   changeFormFieldsState(formFields, true);
